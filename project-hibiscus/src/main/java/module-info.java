51c44b8e269826a5fe81941f7894f0dd58385d0d
Hakushin <77056744+KPX1164@users.noreply.github.com>
2022-09-21 17:10:37 +0700

    Add files via upload

---
+++ project-hibiscus/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module se233.hibiscus.hibiscus {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires zip4j;
+    requires com.google.common;
+
+    opens se233.hibiscus to javafx.fxml;
+    opens se233.hibiscus.controller to javafx.fxml;
+
+    exports se233.hibiscus;
+    exports se233.hibiscus.controller ;
+}
---
+++ project-hibiscus/src/main/java/se233/hibiscus/model/Merger.java
@@ -0,0 +1,4 @@
+package se233.hibiscus.model;
+
+public class Merger {
+}
